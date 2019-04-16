4186dfa1df0a1aa25c856d299faa71635b354e25
Evan Ott <evan1533@vt.edu>
2019-04-16 02:38:46 -0400

    Added honor code to source files

--- src/ASMParser.c
+++ src/ASMParser.c
@@ -1,3 +1,25 @@
+// On my honor:
+//
+// - I have not discussed the C language code in my program with
+// anyone other than my instructor or the teaching assistants
+// assigned to this course.
+//
+// - I have not used C language code obtained from another student,
+// the Internet, or any other unauthorized source, either modified
+// or unmodified.
+//
+// - If any C language code or documentation used in my program
+// was obtained from an authorized source, such as a text book or
+// course notes, that has been clearly noted with a proper citation
+// in the comments of my program.
+//
+// - I have not designed this program in such a way as to defeat or
+// interfere with the normal operation of the Curator System.
+//
+//
+// Evan Ott
+// evan1533
+
 #include <inttypes.h>  // I used functions from these Standard modules in
 #include <string.h>    // my solution; you may or may not need all of them,
 #include <stdlib.h>    // and you might need additional ones, depending on
