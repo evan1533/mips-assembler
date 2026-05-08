3934ef4a5c7e1da24dbd1ce5ea7b6997f77b5aac
Ott, Evan <eott@eab.com>
2026-05-08 11:13:42 -0400

    I don't know what these changes are but I want to save them

--- src/ParseResult.c
+++ src/ParseResult.c
@@ -3,40 +3,40 @@
 #include <string.h>
 
 /**  Frees the dynamic content of a ParseResult object.
- * 
+ *
  *   Pre:  pPR points to a proper ParseResult object.
  *   Post: All of the dynamically-allocated arrays in *pPR have been
  *         deallocated; pointers are NULL, static fields are reset to
  *         default values.
- * 
+ *
  *   Comments:
  *     -  The function has no information about whether *pPR has been
- *        allocated dynamically, so it cannot risk attempting to 
+ *        allocated dynamically, so it cannot risk attempting to
  *        deallocate *pPR.
  *     -  The function is intended to provide the user with a simple
  *        way to free memory; the user may or may not reuse *pPR.  So,
  *        the function does set the pointers in *pPR to NULL.
  */
 void clearResult(ParseResult* const pPR)
-{   
+{
    if(strcmp(pPR->Opcode, "000000") == 0)
    {
       free(pPR->rdName);
-	   free(pPR->RD);
-		free(pPR->Funct);
+       free(pPR->RD);
+        free(pPR->Funct);
    }
    else
    {
-      free(pPR->IMM); 
+      free(pPR->IMM);
    }
 
    free(pPR->ASMInstruction);
-	free(pPR->Mnemonic);
-	free(pPR->rtName);
+    free(pPR->Mnemonic);
+    free(pPR->rtName);
    free(pPR->RT);
    free(pPR->RS);
-	free(pPR->rsName);
-	free(pPR->Opcode);
+    free(pPR->rsName);
+    free(pPR->Opcode);
 
    pPR->ASMInstruction = NULL;
    pPR->Mnemonic = NULL;
@@ -56,34 +56,34 @@ void clearResult(ParseResult* const pPR)
 }
 
 /**  Prints the contents of a ParseResult object.
- * 
+ *
  *   Pre:  Log is open on an output file.
  *         pPR points to a proper ParseResult object.
  */
 void printResult(FILE* Log, const ParseResult* const pPR) {
-   
+
       fprintf(Log, "%s\n", pPR->ASMInstruction);
       fprintf(Log, "   %s   %s\n", pPR->Opcode, pPR->Mnemonic);
       fprintf(Log, "   %2"PRIu8"   %s", pPR->rd, pPR->rdName);
       if ( pPR->RD != NULL ) {
-			fprintf(Log, "   %s", pPR->RD);
-		}
-		fprintf(Log, "\n");
+            fprintf(Log, "   %s", pPR->RD);
+        }
+        fprintf(Log, "\n");
       fprintf(Log, "   %2"PRIu8"   %s", pPR->rs, pPR->rsName);
       if ( pPR->RS != NULL ) {
-			fprintf(Log, "   %s", pPR->RS);
-		}
-		fprintf(Log, "\n");
+            fprintf(Log, "   %s", pPR->RS);
+        }
+        fprintf(Log, "\n");
       fprintf(Log, "   %2"PRIu8"   %s", pPR->rt, pPR->rtName);
       if ( pPR->RT != NULL ) {
-			fprintf(Log, "   %s", pPR->RT);
-		}
-		fprintf(Log, "\n");
+            fprintf(Log, "   %s", pPR->RT);
+        }
+        fprintf(Log, "\n");
       fprintf(Log, "   %s\n", pPR->Funct);
       fprintf(Log, "   %"PRId16"\n", pPR->Imm);
       if ( pPR->IMM != NULL ) {
-			fprintf(Log, "   %s", pPR->IMM);
-		}
-		fprintf(Log, "\n");
-		fprintf(Log, "\n");
+            fprintf(Log, "   %s", pPR->IMM);
+        }
+        fprintf(Log, "\n");
+        fprintf(Log, "\n");
 }
--- src/driver.c
+++ src/driver.c
@@ -32,9 +32,9 @@
 #define NUM_TESTS         25   // modify this if you want to run more/fewer tests
 
 int main(int argc, char** argv) {
-    
+
    uint32_t nTests = NUM_TESTS;
-   
+
    // Validate command-line:
    if ( argc < 3 || argc > 4 ) {
       printf("Invocation:  parse <assembly instruction file> <parser results file> [-repeat]\n");
@@ -56,11 +56,11 @@ int main(int argc, char** argv) {
    else if ( argc == 4 ) {
       printf("Unknown option: %s... ignoring it.\n", argv[3]);
    }
-   
+
    if ( !repeatOn ) {
       GenerateInput(argv[1], nTests);
    }
-   
+
    // Open specified input file and verify it's available:
    FILE* src   = fopen(argv[1], "r");
    if ( src == NULL ) {
@@ -69,47 +69,47 @@ int main(int argc, char** argv) {
    }
    // Open specified results file; taking its creation for granted:
    FILE* results  = fopen(argv[2], "w");
-   
+
    // Variables for instruction processing:
    char asmInstruction[MAX_INSTR_LENGTH + 1];   // holder for current asm instruction
    uint32_t totalScore = 0;                     // student's score for test run
    uint32_t numInstructions = 0;                // assembly instruction count
-   
+
    // Storage for a list of scores on the test cases:
    uint32_t *scores = malloc(nTests*sizeof(uint32_t));
    for (uint32_t idx = 0; idx < nTests; idx++) {
-		scores[idx] = -1;
-	}
+        scores[idx] = -1;
+    }
 
    // Process as long as there are more instructions:
    while ( fgets(asmInstruction, MAX_INSTR_LENGTH + 1, src) != NULL ) {
-      
+
       // erase trailing newline from string
       asmInstruction[strlen(asmInstruction) - 1] = '\0';
-      
+
       // Make backup copy of instruction string for grading:
       char* asmCopy = calloc(MAX_INSTR_LENGTH + 1, sizeof(char));
       strcpy(asmCopy, asmInstruction);
-      
+
       fprintf(results, "Testing with:  %s\n", asmInstruction);
-      
+
       // Invoke student's ASM parser on current instruction:
       ParseResult* studentParse = parseASM(asmInstruction);
-      
+
       // Check for NULL return:
       if ( studentParse == NULL ) {
-			fprintf(results, "Error:  parseASM() returned NULL for \"%s\"\n", asmCopy);
-			return 1;
-		}
-      
+            fprintf(results, "Error:  parseASM() returned NULL for \"%s\"\n", asmCopy);
+            return 1;
+        }
+
       // Compare student's translation to reference translation and score it:
       uint32_t currScore = scoreResult(results, studentParse, asmCopy);
-      
+
       // Update score information:
       totalScore += currScore;
       scores[numInstructions] = currScore;
       numInstructions++;
-      
+
       // Clean up dynamic allocations that should have occurred on this test:
       free(asmCopy);
       clearResult(studentParse);
@@ -119,19 +119,19 @@ int main(int argc, char** argv) {
    // Log score results:
    fprintf(results, "Scores:\n");
    for (uint32_t idx = 0; idx < nTests; idx++) {
-		fprintf(results, "  Test %2"PRIu32":%5"PRIu32"/%"PRIu32"\n", 
-		                           idx + 1, scores[idx], PTS_PER_TEST);
-	}
-	fprintf(results, "\n");
-   fprintf(results, "Total score:  %"PRIu32"/%"PRIu32"\n", 
+        fprintf(results, "  Test %2"PRIu32":%5"PRIu32"/%"PRIu32"\n",
+                                   idx + 1, scores[idx], PTS_PER_TEST);
+    }
+    fprintf(results, "\n");
+   fprintf(results, "Total score:  %"PRIu32"/%"PRIu32"\n",
                                      totalScore, nTests * PTS_PER_TEST);
-   
+
    // Clean up last allocation:
    free(scores);
-   
+
    // Close input/output files:
    fclose(src);
    fclose(results);
-   
+
    return 0;
 }
